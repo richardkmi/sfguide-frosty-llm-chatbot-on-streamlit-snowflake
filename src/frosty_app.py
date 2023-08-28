1f8260344015614bbf5f7cb9b04bab2a1205c30f
richardkmi <83045076+richardkmi@users.noreply.github.com>
2023-08-27 19:55:54 -0700

    remove hardcoded pip versions

--- src/frosty_app.py
+++ src/frosty_app.py
@@ -3,7 +3,7 @@ import re
 import streamlit as st
 from prompts import get_system_prompt
 
-st.title("☃️ Frosty")
+st.title("🦈 HDB Chat")
 
 # Initialize the chat messages history
 openai.api_key = st.secrets.OPENAI_API_KEY
